934337fdf18a927b066e2636d3c10a189850f80b
meads58 <meads109@gmail.com>
2015-02-08 13:40:11 +0000

    plane can land and is flying when created

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,12 +1,24 @@
+require 'plane'
+
 describe 'Plane' do
 
+	let(:plane) { Plane.new }
+
   it 'should be flying when created' do 
-    expect(plane.flying?).to be_true
+    expect(plane.flying?).to be true
   end
 
   it 'can land' do
     plane.land!
-    expect(plane.flying?).to be_false
+    expect(plane.flying?).to be false
   end
+
+  it 'can take off' do
+  	plane.land!
+  	plane.take_off
+  	expect(plane.flying?).to be true
+  end
+
+  
 
 end
